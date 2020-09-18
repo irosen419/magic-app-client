60e68e1d6ab85457c824a8e7ba16afc08acf6b8a
irosen419 <irosen419@gmail.com>
2020-09-17 23:24:48 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    const options = {
+      method: 'GET',
+      headers: {
+        Accept: "application/json",
+        Authorization: "bearer rgjZax3N9Gat3Rgbs7DliLkFn0lUPNFfFQjggMh6PcYZjXKLvrsMB6mml2yvWhh7idD6jzWz1EmiTtOmu5JTcwzbSm2jd_beBkZHQGCMV0SIGUa7N5z3dBaXZ64tcUYfhafPZw7YyNZopa0T8eBB4LHcevS-eTZnA9k7uB1DK1HwwVozHbLZb6WKt-E1DeMv7p8bFOoGYJ6ziVo-Pdy6d2gAXe0naaDKYMxA3kjGPOU0B6b2PkPyeQvXkuLJi41GMmJiuIzdkQ9V5lkY7UXjW5-5Lz43NvABAUha_FV5dcD4gNiA9UYaqlWsTqxuz4U4ZzJ6dg"
+      }
+    }
+    fetch('https://api.tcgplayer.com/pricing/product/83461,101491,130325,138608', options)
+      .then(resp => resp.json())
+      .then(console.log)
+  }
+  render() {
+    return (
+      <h1> MTG</h1 >
+    );
+  }
 }
 
 export default App;
